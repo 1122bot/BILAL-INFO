689702dd7dfd849bc659d9eab8fab5c834705e88
1122bot <herokuteam4@gmail.com>
2026-01-21 10:10:06 +0500

    👑 BILAL-MD 👑

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'info.html'));
 });
 
 app.listen(port, () => {
